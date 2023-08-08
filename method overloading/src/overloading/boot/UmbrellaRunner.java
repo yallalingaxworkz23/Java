e442c276dada41bf945475f94998c6ce86c0f091
Yallu3630 <yallu3630@gmail.com>
2023-08-08 10:20:02 +0530

    method overload

---
+++ method overloading/src/overloading/boot/UmbrellaRunner.java
@@ -0,0 +1,13 @@
+package overloading.boot;
+
+import overloading.app.Umbrella;
+
+public class UmbrellaRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Umbrella umbrella=new Umbrella();
+     umbrella.rain();
+	}
+
+}
